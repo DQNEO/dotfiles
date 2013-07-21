b32753829cafaf7dfb9e4ead858731879c22c99e
DQNEO <dqneoo@example.com>
2013-07-22 01:50:42 +0900

    install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+#echo $THIS_DIR
+
+cd ~
+
+for file in .screenrc .emacs .zshrc .gitconfig
+do
+  echo ln -s dotfiles/$file .
+  ln -s dotfiles/$file .
+done
+
+echo dotfiles/.gitconfig.local.template ~/.gitconfig.local
+cp   dotfiles/.gitconfig.local.template ~/.gitconfig.local
+
+echo "edit .gitconfig.local manually"
+
