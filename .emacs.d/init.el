e5c164f55764bb793a8a3b6d4fc48ff3a9ff669f
DQNEO <dqneoo@example.com>
2013-07-24 02:51:34 +0900

    init.elを作成。melpaとmarmaladeをレポジトリとして追加。

---
+++ .emacs.d/init.el
@@ -0,0 +1,8 @@
+;; http://d.hatena.ne.jp/naoya/20130107/1357553140
+;; Emacs 24 の初期状態ではパッケージ提供元が elpa.gnu.org のみでやや数が少ないので
+;; 下記のように設定して増やしてあげると良いでしょう。
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
