741a9c79f12f464696712d5c65a7490aae9adf75
DQNEO <dqneoo@gmail.com>
2013-10-22 22:42:08 +0900

    auto chmod

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,21 @@
 ; http://yoshiori.github.io/blog/2013/01/31/file-update-emacs/
 ; 変更のあったファイルの自動再読み込み
 (global-auto-revert-mode 1)
+
+;;=============== auto chmod ===============
+;; ファイル保存時にchmodする
+;; chmod +x when you save a file
+;; http://www.namazu.org/~tsuchiya/elisp/index.html#chmod
+(defun make-file-executable ()
+  "Make the file of this buffer executable, when it is a script source."
+  (save-restriction
+    (widen)
+    (if (string= "#!" (buffer-substring-no-properties 1 (min 3 (point-max))))
+        (let ((name (buffer-file-name)))
+          (or (equal ?. (string-to-char (file-name-nondirectory name)))
+              (let ((mode (file-modes name)))
+                (set-file-modes name (logior mode (logand (/ mode 4) 73)))
+                (message (concat "Wrote " name " (+x)"))))))))
+(add-hook 'after-save-hook 'make-file-executable)
+
+
