3ad3716234339effc5b9643c81a168358bfab01a
DQNEO <dqneoo@gmail.com>
2013-10-23 22:04:05 +0900

    introduce js2-mode (set indent 2)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -97,3 +97,18 @@
 ;; http://d.hatena.ne.jp/kyagi/20090601/1243841415
 (require 'shell-pop)
 (global-set-key [f8] 'shell-pop)
+
+;;=============== js2-mode ================
+;; http://memememomo.hatenablog.com/entry/2013/03/23/125240
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
+
+;; http://blog.ainam.me/2011/12/13/emacs-js2-mode-indent/
+;; インデントの関数の再設定
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (require 'js)
+              (setq js-indent-level 2
+                    js-expr-indent-offset 2
+                    indent-tabs-mode nil)
+              (set (make-local-variable 'indent-line-function) 'js-indent-line)))
