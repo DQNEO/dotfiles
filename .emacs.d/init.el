5c6a9df47f8ac558d338e6d859ad0c2d6bb1be37
DQNEO <dqneoo@gmail.com>
2013-11-06 22:45:38 +0900

    add swap-screen

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,3 +118,27 @@
                     indent-tabs-mode nil)
               (set (make-local-variable 'indent-line-function) 'js-indent-line)))
 
+;;=============== swap-screen ===============
+;; これは超便利。
+;; 
+;; http://www.bookshelf.jp/soft/meadow_30.html#SEC405
+;; exchange right <->left window
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+(global-set-key [f2] 'swap-screen)
+(global-set-key [S-f2] 'swap-screen-with-cursor)
+
+(global-set-key "\C-cx" 'swap-screen)
