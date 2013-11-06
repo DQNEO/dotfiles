960b74a6f82daa413b2bc76b81e8ee7697c2791d
DQNEO <dqneoo@gmail.com>
2013-11-06 22:42:26 +0900

    kaizen comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,8 @@
 ;; Packages
 ;; http://d.hatena.ne.jp/naoya/20130107/1357553140
-;; Emacs 24 の初期状態ではパッケージ提供元が elpa.gnu.org のみでやや数が少ないので
-;; 下記のように設定して増やしてあげると良いでしょう。
+;; Emacs 24 のデフォルトではパッケージ提供元が
+;; elpa.gnu.org のみなので
+;; 下記のように設定して増やしてあげる
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
