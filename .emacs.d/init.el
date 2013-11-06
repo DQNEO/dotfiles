e5ad36afc0a59b53af54157df3e6fdfc13c7f249
DQNEO <dqneoo@gmail.com>
2013-11-06 22:57:34 +0900

    adjust

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,6 +64,9 @@
 ;; http://www.cozmixng.org/~kou/emacs/dot_emacs
 (setq kill-whole-line t)
 
+
+;;============ Global Variables ====================
+
 ;; recentf
 ;; http://www23.atwiki.jp/selflearn/pages/41.html#id_4af821e2
 (require 'recentf)
