4174be6d9e870ddc74c4efbb5b5c58d183431b01
DQNEO <dqneoo@gmail.com>
2013-11-23 23:27:20 +0900

    環境変数GIT_PAGERを設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -148,6 +148,9 @@
 (global-set-key "\C-xf" 'recentf-open-files)
 
 ;; Git
+; ページャをEmacs用にカスタマイズする
+(setenv "GIT_PAGER" "nkf -w|colordiff")
+
 ; EmacsのGitクライアント：Magit
 ; http://qiita.com/takc923/items/c7a11ff30caedc4c5ba7
 (require 'magit)
