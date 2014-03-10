c876c865630ab721222e776cbf7cac925c6a4984
DQNEO <dqneoo@gmail.com>
2014-03-10 09:01:40 +0900

    仕事の.emacsとの差分解消

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,11 +15,11 @@
 
 ;; Language
 (set-language-environment 'Japanese)
-(set-default-coding-systems 'utf-8)
 (set-keyboard-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8-unix)
-(set-buffer-file-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
+(set-buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8)
 
 ;; Packages
 ;; http://d.hatena.ne.jp/naoya/20130107/1357553140
