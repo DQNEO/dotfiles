d9db4de527f37726e2bd30eb086283842fd5a56e
DQNEO <dqneoo@gmail.com>
2014-03-24 21:23:55 +0900

    cosmetic

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -231,7 +231,7 @@
 ;;=============== shell-mode ===============
 ;; http://www.namazu.org/~tsuchiya/elisp/index.html#script
 (autoload 'ansi-color-for-comint-mode-on "ansi-color"
-          "Set `ansi-color-for-comint-mode' to t." t)
+  "Set `ansi-color-for-comint-mode' to t." t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
 
