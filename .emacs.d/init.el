028f2792fb1ad9bacb315d77a57b459596fa702f
DQNEO <dqneoo@gmail.com>
2014-03-24 21:09:44 +0900

    仕事/個人の差異がある部分は上の方によせる

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,13 +13,14 @@
 ;; M-h
 ")
 
+(set-buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8)
+
 ;; Language
 (set-language-environment 'Japanese)
 (set-keyboard-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
-(set-buffer-file-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8)
 
 ;; Packages
 ;; http://sheephead.homelinux.org/2011/06/17/6724/
