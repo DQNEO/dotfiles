3a9ce29c63fb6bbc639e5673f3a7926462917448
DQNEO <dqneoo@gmail.com>
2014-03-24 21:26:44 +0900

    仕事用の設定にあわせる

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -239,6 +239,10 @@
 ;; http://www23.atwiki.jp/selflearn/pages/41.html#id_4af821e2
 (require 'recentf)
 (recentf-mode 1)
+;; http://www.clear-code.com/blog/2011/2/16.html
+;;; 最近開いたファイルを保存する数を増やす
+(setq recentf-max-saved-items 10000)
+;; open recent files
 (global-set-key "\C-xf" 'recentf-open-files)
 
 ;; Git
