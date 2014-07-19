551f4051bfbb3aa5b862094c9871ad9c45b7ecc9
DQNEO <dqneoo@gmail.com>
2014-07-19 14:09:40 +0900

    c-modeをカスタマイズして機能追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -309,6 +309,23 @@
 (add-hook 'c-mode-hook
   '(lambda ()
      (c-set-style "stroustrup")
+
+     ;; Emacs で C 言語プログラミングを始める人へのイントロダクション
+     ;; http://at-aka.blogspot.com/2006/12/emacs-c.html
+
+     ;; センテンスの終了である ';' を入力したら、自動改行+インデント
+     (c-toggle-auto-hungry-state 1)
+     ;; RET キーで自動改行+インデント
+     (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+
+     ;; (flyspell-prog-mode)
+
+     ;; backspaceで空白を一気に削除
+     (setq c-hungry-delete-key t)
+
+     (setq tab-width 4)
+     (setq c-basic-offset 4)
+     (setq indent-tabs-mode nil)
      ))
 
 ;;=============== php-mode ===============
