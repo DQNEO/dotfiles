f29e05f31a3a51e21f2af8483a64759cbb2270c0
DQNEO <dqneoo@gmail.com>
2014-07-21 17:28:32 +0900

    copied from my cheese settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -334,6 +334,15 @@
              (setq php-search-url "http://php.net")
              (setq php-manual-url "http://www.phppro.jp/phpmanual")
 
+      ;; (require 'php-completion)
+      ;;       (php-completion-mode t)
+      ;;       (define-key php-mode-map (kbd "C-o") 'phpcmp-complete) ;php-completionの補完実行キーバインドの設定
+      ;;       (make-local-variable 'ac-sources)
+      ;;       (setq ac-sources '(
+      ;;                          ac-source-words-in-same-mode-buffers
+      ;;                          ac-source-php-completion
+      ;;                          ac-source-filename
+      ;;                          ))
              )
 
           )
