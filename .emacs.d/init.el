5934b1d21451d77a58a9d66de0d93213d932b5f8
DQNEO <dqneoo@gmail.com>
2014-07-21 17:12:11 +0900

    DRY

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -254,10 +254,6 @@
 (ansi-color-for-comint-mode-on)
 
 
-;; Git
-; ページャをEmacs用にカスタマイズする
-(setenv "GIT_PAGER" "nkf -w|colordiff")
-
 ; EmacsのGitクライアント：Magit
 ; http://qiita.com/takc923/items/c7a11ff30caedc4c5ba7
 (require 'magit)
