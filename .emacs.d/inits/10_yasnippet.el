df03ebaa418ddd0997205b5fcb187e87f1aef817
DQNEO <dqneoo@gmail.com>
2015-02-08 13:09:59 +0900

    use yasnippet (ただし使い方は忘れた)

---
+++ .emacs.d/inits/10_yasnippet.el
@@ -0,0 +1,13 @@
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/mysnippets"
+        "~/.emacs.d/yasnippet-snippets"
+        ))
+(yas-global-mode 1)
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
