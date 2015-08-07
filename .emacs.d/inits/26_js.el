a2813a568818aea00e7269f95082dfa4b72bcbb3
DQNEO <dqneoo@gmail.com>
2015-08-07 21:27:45 +0900

    follow ko san's style

--- .emacs.d/inits/26_js.el
+++ .emacs.d/inits/26_js.el
@@ -8,8 +8,8 @@
 (add-hook 'js2-mode-hook
           #'(lambda ()
               (require 'js)
-              (setq js-indent-level 2
-                    js-expr-indent-offset 2
+              (setq js-indent-level 4
+                    js-expr-indent-offset 4
                     indent-tabs-mode nil)
               (set (make-local-variable 'indent-line-function) 'js-indent-line)))
 
