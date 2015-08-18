a3e561c7ebcc5ed37c53a7b1fb5581bd0eba1fa7
DQNEO <dqneoo@gmail.com>
2015-08-18 11:07:11 +0900

    add tig

--- brew_install.sh
+++ brew_install.sh
@@ -2,6 +2,6 @@
 
 # 現在入ってるパッケージ一覧は`brew ls`で列挙できる
 brew install zsh emacs git cask
-brew install hub ghq peco z
+brew install hub ghq peco z tig
 brew install node
 brew install coreutils findutils  gnu-sed
