f3b569661e55f1cc91931ca834cbe2fa02ffa721
DQNEO <dqneoo@gmail.com>
2015-09-28 01:38:37 +0900

    use cperl-mode instead of perl-mode

---
+++ .emacs.d/inits/20_perl.el
@@ -0,0 +1,18 @@
+;; copied from http://memememomo.hatenablog.com/entry/2013/03/17/182626
+(defalias 'perl-mode 'cperl-mode)
+(setq auto-mode-alist (append '(("\\.psgi$" . cperl-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.cgi$" . cperl-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.pl$" . cperl-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.pm$" . cperl-mode)) auto-mode-alist))
+
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+
+             ;; インデント設定
+             (cperl-set-style "PerlStyle")
+             (custom-set-variables
+              '(cperl-indent-parens-as-block t)
+              '(cperl-close-paren-offset -4)
+              '(cperl-indent-subs-specially nil))
+
+             ))
