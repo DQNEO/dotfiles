73a66bb51e86618f4fe98a742ba005a57dd4eded
DQNEO <dqneoo@gmail.com>
2015-10-14 12:10:57 +0900

    add zsh-completions

--- brew_install.sh
+++ brew_install.sh
@@ -2,6 +2,7 @@
 
 # 現在入ってるパッケージ一覧は`brew ls`で列挙できる
 brew install zsh emacs git cask
+brew install zsh-completions
 brew install hub ghq peco z tig
 brew install node
 brew install coreutils findutils  gnu-sed
