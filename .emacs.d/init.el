c8245ffa1ab222cca93014efb9ab2cef3268c435
DQNEO <dqneoo@gmail.com>
2017-06-04 23:09:22 -0700

    auto prune on fetch/pull

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,13 @@
 
 ;; Cask
 ;; OSX(Homebrew)の場合
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(package-initialize)
+
 (require 'cask)
 ;; Linux の場合はcask.elのパスを指定する必要がある。
 ;; (require 'cask "~/.cask/cask.el")
