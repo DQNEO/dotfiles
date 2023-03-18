14b2b00dcade8929ec92ef5f93e93484048bf627
DQNEO <dqneoo@gmail.com>
2023-03-18 17:15:22 +0900

    Automatically inserted code (I don't know where this came from)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,3 +38,15 @@
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(init-loader)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
